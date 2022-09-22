1bad548cb3384eca7f42b229aa245343a7875529
ArnaudBD <abiju-du@student.42.fr>
2022-09-22 18:00:28 +0200

    feat: wordpress starts in browser and it connects to mariadb if you fill the form

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -1,6 +1,11 @@
-# !/bin/bash
-mysqld << EOF
+#! /bin/bash
+mysqld &
+PID=$!
+sleep 5
+mariadb << EOF
 CREATE USER abd IDENTIFIED BY 'chut';
 CREATE DATABASE wordpress;
 GRANT ALL PRIVILEGES ON wordpress.* to abd;
 EOF
+kill $PID
+exec mysqld
