4ec0b18307f4054f220a972a1df4fcc225e8e38c
ArnaudBD <abiju-du@student.42.fr>
2022-09-26 20:07:37 +0200

    wip: trying to set wordpress admin user with wp-cli

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -4,6 +4,7 @@
 sleep 10
 # if exist -------------
 cd /var/www/wordpress && wp config create --dbuser=abd --dbpass=chut --dbhost=maria_debe --dbname=wordpress --allow-root
-cd /var/www/wordpress && wp faitlereste
+# cd /var/www/wordpress && wp user create abiju-du abiju-du@student.42.fr --role=administrator --send-email --porcelain
+cd /var/www/wordpress && wp core install --title="Abiju-du's website" --admin_user="abiju-du" --admin_password="123" --admin_email="abiju-du@student.42.fr" #--url="http://abiju-du.42.fr"
 # kill $PID
 exec php-fpm7.4 -F -R
