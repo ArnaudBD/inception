6f78a4a680a9ebf3bd15bd317dcdf7535fac1a7d
ArnaudBD <arnaud.bijuduval@gmail.com>
2022-09-29 08:52:58 +0200

    feat: wordpress admin user is configured

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -3,8 +3,8 @@
 # PID=&!
 sleep 10
 # if exist -------------
-cd /var/www/wordpress && wp config create --dbuser=abd --dbpass=chut --dbhost=maria_debe --dbname=wordpress --allow-root
+cd /var/www/wordpress && wp config create --dbuser=abd --dbpass=chut --dbhost=maria_debe --dbname=wordpress --allow-
 # cd /var/www/wordpress && wp user create abiju-du abiju-du@student.42.fr --role=administrator --send-email --porcelain
-cd /var/www/wordpress && wp core install --title="Abiju-du's website" --admin_user="abiju-du" --admin_password="123" --admin_email="abiju-du@student.42.fr" #--url="http://abiju-du.42.fr"
+cd /var/www/wordpress && wp core install --title="Abiju-du's website" --admin_user="abiju-du" --admin_password="123" --admin_email="abiju-du@student.42.fr" --url=abiju-du.42.fr --allow-root  #--url="http://abiju-du.42.fr"
 # kill $PID
 exec php-fpm7.4 -F -R
