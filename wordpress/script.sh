f91b7c21fa618bb9f022977278c3cbf6beff898f
ArnaudBD <arnaud.bijuduval@gmail.com>
2022-10-03 09:05:51 -0400

    feat: works in VM

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 # php-fpm7.4  &
 # PID=&!
-sleep 10
+sleep 50
 # if exist -------------
-cd /var/www/wordpress && wp config create --dbuser=abd --dbpass=chut --dbhost=maria_debe --dbname=wordpress --allow-
+cd /var/www/wordpress
+wp config create --dbuser=abd --dbpass=chut --dbhost=maria_debe --dbname=wordpress --allow-root
 # cd /var/www/wordpress && wp user create abiju-du abiju-du@student.42.fr --role=administrator --send-email --porcelain
-cd /var/www/wordpress && wp core install --title="Abiju-du's website" --admin_user="abiju-du" --admin_password="123" --admin_email="abiju-du@student.42.fr" --url=abiju-du.42.fr --allow-root  #--url="http://abiju-du.42.fr"
+wp core install --title="Abiju-du's website" --admin_user="abiju-du" --admin_password="123" --admin_email="abiju-du@student.42.fr" --url=abiju-du.42.fr --allow-root  #--url="http://abiju-du.42.fr"
 # kill $PID
 exec php-fpm7.4 -F -R
