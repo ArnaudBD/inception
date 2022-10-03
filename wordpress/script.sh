4615e7801c8ecacb5468df75616f4bbd8eb03382
ArnaudBD <arnaud.bijuduval@gmail.com>
2022-10-03 21:12:57 +0200

    feat: user created in wp

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -5,7 +5,7 @@ sleep 50
 # if exist -------------
 cd /var/www/wordpress
 wp config create --dbuser=abd --dbpass=chut --dbhost=maria_debe --dbname=wordpress --allow-root
-# cd /var/www/wordpress && wp user create abiju-du abiju-du@student.42.fr --role=administrator --send-email --porcelain
+wp user create abiju-du user@student.42.fr --role=user
 wp core install --title="Abiju-du's website" --admin_user="abiju-du" --admin_password="123" --admin_email="abiju-du@student.42.fr" --url=abiju-du.42.fr --allow-root  #--url="http://abiju-du.42.fr"
 # kill $PID
 exec php-fpm7.4 -F -R
