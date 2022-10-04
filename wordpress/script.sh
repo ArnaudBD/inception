b3541e610d93a503d880d6ad6ccbbb8c8f07f34c
ArnaudBD <arnaud.bijuduval@gmail.com>
2022-10-04 12:50:46 +0200

    feat: https enabled

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -4,8 +4,8 @@
 sleep 50
 # if exist -------------
 cd /var/www/wordpress
-wp config create --dbuser=abd --dbpass=chut --dbhost=maria_debe --dbname=wordpress --allow-root
-wp user create abiju-du user@student.42.fr --role=user
-wp core install --title="Abiju-du's website" --admin_user="abiju-du" --admin_password="123" --admin_email="abiju-du@student.42.fr" --url=abiju-du.42.fr --allow-root  #--url="http://abiju-du.42.fr"
+wp config create --dbuser=abd --dbpass=chut --dbhost=maria_debe --dbname=wordpress --allow-root 
+wp user create abiju-du user@student.42.fr --role=user --allow-root 
+wp core install --title="Abiju-du's website" --admin_user="abiju-du" --admin_password="123" --admin_email="abiju-du@student.42.fr" --url=abiju-du.42.fr --allow-root  
 # kill $PID
 exec php-fpm7.4 -F -R
