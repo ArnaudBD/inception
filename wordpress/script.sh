5db5b91d0675702fb70b067ebdb6e9662273b34c
ArnaudBD <arnaud.bijuduval@gmail.com>
2022-10-05 15:42:24 +0200

    feat: .env created

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -12,12 +12,14 @@ fi
 wp user get abd --allow-root --quiet
 if [ ! -f 'wp-config.php' ]
 	then
-	wp config create --dbuser=abd --dbpass=chut --dbhost=maria_debe --dbname=wordpress --allow-root 
+	wp config create --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=maria_debe --dbname=wordpress --allow-root 
 fi
 wp user get abiju-du --allow-root --quiet
 if [ $? -ne 0 ]
 	then
-	wp core install --title="Abiju-du's website" --admin_user="abiju-du" --admin_password="123" --admin_email="abiju-du@student.42.fr" --url=abiju-du.42.fr --allow-root
+	wp core install --title="Abiju-du's website" --admin_user=$ADMINUSER --admin_password=$ADMINPASSWORD --admin_email="abiju-du@student.42.fr" --url=abiju-du.42.fr --allow-root
 fi
+# wp plugin install redis-cache is-installed
+# wp --path= config set 'variables dans le fichier de config'
 # kill $PID
 exec php-fpm7.4 -F -R
